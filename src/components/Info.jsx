4b5bc6f59185809d133b68da1a37791e1ad843e1
Reginald Wong <reginald_wong@outlook.com>
2023-01-24 19:35:49 -0500

    centered page and styled Info component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import headerImage from "/src/assets/1579800055135.jpeg"
 
 const Info = () => {
     return (
         <header>
-            <img src="src/assets/1579800055135.jpeg" alt="reginald wong" className="header--image" />
+            <img src={headerImage} alt="reginald wong" className="header--image" />
             <h1 className="header--name">Reginald Wong</h1>
             <h3 className="header--title">Full Stack Software Developer</h3>
             <h4 className="header--link"><a href="https://reginaldwong.com" target="_blank">reginaldwong.com</a></h4>
