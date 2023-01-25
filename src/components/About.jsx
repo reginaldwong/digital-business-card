e50d949932353565097b68e62049e82272329ba3
Reginald Wong <reginald_wong@outlook.com>
2023-01-24 23:37:31 -0500

    add icons and finish styling Info component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <section>
             <h2 className="about--title">About</h2>
-           <p className="about--description">I am a full stack developer with a particular interest for design, functionality, and user experience. I  enjoy building software that improves the lives of those around me and I am always looking for new things to learn.</p> 
+            <p className="about--description">I am a full stack developer with a particular interest for design, functionality, and user experience. I  enjoy building software that improves the lives of those around me and I am always looking for new things to learn.</p> 
         </section>
     )
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import headerImage from "/src/assets/1579800055135.jpeg"
+import mailIcon from "/src/assets/Mail.png"
 
 const Info = () => {
     return (
@@ -8,7 +9,7 @@ const Info = () => {
             <h1 className="header--name">Reginald Wong</h1>
             <h3 className="header--title">Full Stack Software Developer</h3>
             <h4 className="header--link"><a href="https://reginaldwong.com" target="_blank">reginaldwong.com</a></h4>
-            <button><a href="mailto:reginaldwongdev@gmail.com">Email</a></button>
+            <button className="header--button"><img  src={mailIcon}/><a href="mailto:reginaldwongdev@gmail.com" className="header--button--link">Email</a></button>
         </header>
     )
 }
