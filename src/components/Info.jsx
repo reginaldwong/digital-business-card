95d5a0569c8f218fa508e2e06369db94e0ebb0ed
Reginald Wong <reginald_wong@outlook.com>
2023-01-24 23:53:31 -0500

    style About and Interests components

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,7 +7,7 @@ const Info = () => {
         <header>
             <img src={headerImage} alt="reginald wong" className="header--image" />
             <h1 className="header--name">Reginald Wong</h1>
-            <h3 className="header--title">Full Stack Software Developer</h3>
+            <h3 className="header--title">Software Developer</h3>
             <h4 className="header--link"><a href="https://reginaldwong.com" target="_blank">reginaldwong.com</a></h4>
             <button className="header--button"><img  src={mailIcon}/><a href="mailto:reginaldwongdev@gmail.com" className="header--button--link">Email</a></button>
         </header>
