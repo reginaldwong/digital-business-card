8da566e6f46d46455cf4972a740f6168bf1dd2ee
Reginald Wong <reginald_wong@outlook.com>
2023-01-26 08:54:30 -0500

    fix margins

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -3,8 +3,8 @@ import React from "react"
 const Interests = () => {
     return (
         <section>
-        <h2 className="interests--title">Interests</h2>
-        <p className="interests--description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus sequi, amet debitis impedit minus adipisci magnam unde?</p>  
+        <h2 className="interests--title">Currently available!</h2>
+        <p className="interests--description">I am currently interested in full time and freelance opportunities. However, if you have any other requests or questions, do not hesitate to get in touch!</p>  
         </section>
     )
 }
